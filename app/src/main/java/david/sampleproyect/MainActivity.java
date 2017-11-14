b8433a8ca3fb9f8ada84d2750f5613f2d9d41230
davidtrigo <d.trigo83@gmail.com>
2017-11-14 10:37:53 +0100

    update

--- app/src/main/java/david/sampleproyect/MainActivity.java
+++ app/src/main/java/david/sampleproyect/MainActivity.java
@@ -8,61 +8,92 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends Activity {
+
+    private final static int REQUEST_CODE_ACTIVITY = 0;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        TextView textView = findViewById(R.id.textview1);
+
+        textView = findViewById(R.id.textview1);
         textView.setText("Esta es la activity 1");
+
         Button botonNuevaActivity = findViewById(R.id.buttonNewActivity);
 
-        botonNuevaActivity.setOnClickListener(new View.OnClickListener(){
-           @Override
-            public  void onClick(View v){
-              Log.d(MainActivity.class.getSimpleName(),"onClick");
-               Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+        botonNuevaActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(MainActivity.class.getSimpleName(), "onClick");
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                Date currentDate = Calendar.getInstance().getTime();
+                intent.putExtra("date", currentDate.toString());
+                //startActivity(intent);
+                startActivityForResult(intent, REQUEST_CODE_ACTIVITY);
 
-               startActivity(intent);
-           }
+
+            }
 
 
         });
-        Log.d(MainActivity.class.getSimpleName(),"onCreate");
+        Log.d(MainActivity.class.getSimpleName(), "onCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(MainActivity.class.getSimpleName(),"onStart");
+        Log.d(MainActivity.class.getSimpleName(), "onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(MainActivity.class.getSimpleName(),"onResume");
+        Log.d(MainActivity.class.getSimpleName(), "onResume");
     }
 
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(MainActivity.class.getSimpleName(),"onStop");
+        Log.d(MainActivity.class.getSimpleName(), "onStop");
     }
 
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(MainActivity.class.getSimpleName(),"onDestroy");
+        Log.d(MainActivity.class.getSimpleName(), "onDestroy");
     }
 
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(MainActivity.class.getSimpleName(),"onPause");
+        Log.d(MainActivity.class.getSimpleName(), "onPause");
+    }
+
+    /**/
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE_ACTIVITY) {
+
+            Toast.makeText(MainActivity.this, "¡Intent devuelto!", Toast.LENGTH_LONG).show();
+
+            if (data.getExtras() != null && data.getExtras().containsKey("date")) {
+                String fecha = data.getExtras().getString("date");
+                textView.setText(fecha);
+            }
+
+        }
     }
 }
--- app/src/main/java/david/sampleproyect/SecondActivity.java
+++ app/src/main/java/david/sampleproyect/SecondActivity.java
@@ -1,8 +1,15 @@
 package david.sampleproyect;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by A8Alumno on 13/11/2017.
@@ -14,5 +21,28 @@ public class SecondActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
+
+        if (getIntent().getExtras() != null && getIntent().getExtras().containsKey("date")) {
+
+            TextView lblMensajeRecibido = findViewById(R.id.lblMensajeRecibido);
+            String fecha = getIntent().getExtras().getString("date"); // id de Intent.putExtra("date",currentDate.toString());  en MainActivity
+            lblMensajeRecibido.setText(fecha);
+
+
+        }
+
+        Button btndevolverresultado = findViewById(R.id.btndevolverresultado);
+        btndevolverresultado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent resultIntent = new Intent();
+                Date currentDate = Calendar.getInstance().getTime();
+                resultIntent.putExtra("date", currentDate.toString());
+                setResult(RESULT_OK, resultIntent);
+                finish();
+
+            }
+        });
     }
 }
